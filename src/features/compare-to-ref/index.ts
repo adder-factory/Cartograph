50af5aae5fb2c7f540d03091fc93e3fb6614839e
andreinknv <261684394+andreinknv@users.noreply.github.com>
2026-06-06 19:23:51 -0400

    Extract compare-to-ref CLI runner feature

---
+++ src/features/compare-to-ref/index.ts
@@ -0,0 +1,8 @@
+export { makeCompareToRefMcpRunner, type CompareToRefRunnerDeps } from './cli.js';
+export {
+  changedSinceDriftHint,
+  isEmptyCompareToRefText,
+  renderCompareToRefCapture,
+  type CompareToRefCapture,
+  type CompareToRefRender,
+} from './runtime.js';
